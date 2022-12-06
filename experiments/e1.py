80dd62af0b64217e07dd652c64b70a0214330d0a
zhangruihan1 <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-12-06 18:59:56 +0000

    run

--- experiments/e1.py
+++ experiments/e1.py
@@ -13,43 +13,43 @@ from utils import nets, datasets, iterate, misc
 m = nets.auto_net(1).cuda()
 writer = SummaryWriter(comment=m._get_name() + '_train_val')
 
-for epoch in range(1):
+for epoch in range(100):
     m = iterate.train(m,
-        steps.trades_step,
+        steps.auto_step,
         noise_level = 0.6,
-        sample_ = sample_uniform_linf_with_clamp,
-        num = 2,
-        eps = 0.1,
-        microbatch_size = 10000,
-        threshold = 0.9,
+        # sample_ = sample_uniform_linf_with_clamp,
+        # num = 2,
+        # eps = 0.1,
+        # microbatch_size = 10000,
+        # threshold = 0.9,
         device = 'cuda',
-        train_set = datasets.auto_set('MNIST', download=True, train = False, transform=misc.transforms_1),
+        train_set = datasets.auto_set('MNIST', download=True, train = True, transform=misc.transforms_1),
         batch_size = 1000,
         optimizer = torch.optim.Adam(m.parameters(), lr = 0.001),
         epoch = epoch,
         writer = writer,
-        atk = torchattacks.TPGD(m, eps=8/255, alpha=2/255, steps=10)
+        # atk = torchattacks.TPGD(m, eps=8/255, alpha=2/255, steps=10)
     )
 
-    # iterate.validate(m,
-    #     steps.auto_step,
-    #     device = 'cuda',
-    #     val_set = datasets.auto_set('MNIST', download=True, train = False, transform=misc.transforms_1),
-    #     batch_size = 1000,
-    #     epoch = epoch,
-    #     writer = writer
-    # )
+    iterate.validate(m,
+        steps.auto_step,
+        device = 'cuda',
+        val_set = datasets.auto_set('MNIST', download=False, train = False, transform=misc.transforms_1),
+        batch_size = 1000,
+        epoch = epoch,
+        writer = writer
+    )
 
-    # iterate.attack(m,
-    #     steps.ordinary_step,
-    #     steps.auto_step,
-    #     device = 'cuda',
-    #     val_set = datasets.auto_set('MNIST', download=True, train = False, transform=misc.transforms_1),
-    #     batch_size = 1000,
-    #     epoch = epoch,
-    #     writer = writer,
-    #     atk=torchattacks.PGD(m, eps=0.1, alpha=1/255, steps=40, random_start=False),
-    # )
+    iterate.attack(m,
+        steps.ordinary_step,
+        steps.auto_step,
+        device = 'cuda',
+        val_set = datasets.auto_set('MNIST', download=False, train = False, transform=misc.transforms_1),
+        batch_size = 1000,
+        epoch = epoch,
+        writer = writer,
+        atk=torchattacks.PGD(m, eps=0.1, alpha=1/255, steps=40, random_start=False),
+    )
 
     # iterate.attack(m,
     #     steps.ordinary_step,
@@ -69,12 +69,12 @@ torch.save(m.state_dict(), "checkpoints/" + m._get_name() + ".pt")
 outputs = iterate.predict(m,
         steps.auto_step,
         device = 'cuda',
-        val_set = datasets.auto_set('MNIST', download=True, train = False, transform=misc.transforms_1),
+        val_set = datasets.auto_set('MNIST', download=False, train = False, transform=misc.transforms_1),
         batch_size = 100,
         predict = True
 )
 
-print(outputs.keys(), outputs['predictions'])
+# print(outputs.keys(), outputs['predictions'])
 writer.flush()
 writer.close()
 
