05153585f65093cdb4bbb9e3e9947f010c3e11ad
zhangruihan1 <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-12-07 13:31:20 +0000

    config

--- experiments/e1.py
+++ experiments/e1.py
@@ -17,7 +17,7 @@ config = {
 	'num':50,
 	'eps':0.3,
 	'microbatch_size':10000,
-	'threshold':0.9,
+	'threshold':0.95,
 	'device':'cuda',
 	'validation_step':'augmented_step',
 	'attacked_step':'attacked_step'
@@ -49,7 +49,7 @@ for epoch in range(300):
 		val_set = val_set,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.PGD(m, eps=0.3, alpha=0.1, steps=10, random_start=False),
+		atk = torchattacks.PGD(m, eps=config['eps'], alpha=0.1, steps=10, random_start=False),
 	#     atk = torchattacks.PGDL2(m, eps=0.5, alpha=0.2, steps=40, random_start=True),
 		**config
 	)
