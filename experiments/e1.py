925e20a0fb82c5f4da8b4fde03cabdd4ae3c2f54
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-09 04:43:27 +0800

    Update e1.py

--- experiments/e1.py
+++ experiments/e1.py
@@ -11,7 +11,7 @@ from utils import nets, datasets, iterate, misc
 config = {
 	'dataset':'MNIST',
 	'training_step':'our_step',
-	'batch_size':8,
+	'batch_size':32,
 # 	'noise_level':0.6,
 	'sample_':'sample_uniform_linf_with_clamp',
 	'num':50,
