1f49b79fb0ab8f5f517ca65534ca1db94a4abff0
zhangruihan1 <zrh15832168740@gmail.com>
2022-12-12 17:41:23 +0800

    mnist

--- experiments/e1.py
+++ experiments/e1.py
@@ -12,43 +12,85 @@ config = {
 	'dataset':'MNIST',
 	'training_step':'our_step',
 	'batch_size':32,
-# 	'noise_level':0.6,
+	# 'optimizer':'SGD',
+	# 'optimizer_config':{
+	# 	'lr':0.1,
+	# 	'momentum':0.9,
+	# 	'weight_decay':2e-4,
+	# },
+	'optimizer':'Adadelta',
+	'optimizer_config':{
+		'lr':1,
+	},
+	# 'optimizer':'AdamW',
+	# 'optimizer_config':{
+	# 	'weight_decay':0,
+	# },
+	'scheduler':'MultiStepLR',
+	'scheduler_config':{
+		'milestones':[75,90,100],
+		'gamma':1,
+	},
+	# 'noise_level':0.6,
 	'sample_':'sample_uniform_linf_with_clamp',
-	'num':50,
+	'num':50,	
 	'eps':0.3,
+	'attack':'PGD',
+	'attack_config':{
+		'eps':0.3,
+		'alpha':0.1,
+		'steps':10,
+		'random_start':False,
+	},
+	# 'attack':'PGDL2',
+	# 'attack_config':{
+	# 	'eps':0.5, #PGD
+	# 	'alpha':0.2,
+	# 	'steps':40,
+	# 	'random_start':True,
+	# }
 	'microbatch_size':10000,
 	'threshold':0.95,
+	'adversarial':'TPGD',
+	'adversarial_config':{
+		'eps':0.3,
+		'alpha':0.1,
+		'steps':7,
+	},
 	'device':'cuda',
 	'validation_step':'augmented_step',
 	'attacked_step':'attacked_step'
-	}
+}
 
 train_set, val_set, channel = misc.auto_sets(config['dataset'])
 m = nets.auto_net(channel).cuda()
 
 writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config['training_step']}")
 # writer.add_hparams(config, {})
-optimizer = torch.optim.Adadelta(m.parameters(), lr = 1)
-# optimizer = torch.optim.Adam(m.parameters(), lr = 1e-3)
 
 import json
 with open("checkpoints/configs.json", 'a') as f:
 	f.write(json.dumps({**{'run':writer.log_dir.split('/')[-1]}, **config}) + '\n')
+	print(json.dumps(config, indent=4))
 
 for k, v in config.items():
 	if k.endswith('_step'):
 		config[k] = vars(steps)[v]
 	elif k == 'sample_':
 		config[k] = vars(sampling)[v]
-
+	elif k == 'optimizer':
+		config[k] = vars(torch.optim)[v](m.parameters(), **config[k+'_config'])
+		config['scheduler'] = vars(torch.optim.lr_scheduler)[config['scheduler']](config[k], **config['scheduler_config'])
+	elif k == 'adversarial' or k == 'attack':
+		config[k] = vars(torchattacks)[v](m, **config[k+'_config'])
+		
 
 for epoch in range(300):
 	iterate.train(m,
 		train_set = train_set,
-		optimizer = optimizer,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.TPGD(m, eps=config['eps'], alpha=0.1, steps=7),
+		atk = config['adversarial'],
 		**config
 	)
 
@@ -56,11 +98,11 @@ for epoch in range(300):
 		val_set = val_set,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.PGD(m, eps=config['eps'], alpha=1/255, steps=400, random_start=False),
-	#     atk = torchattacks.PGDL2(m, eps=0.5, alpha=0.2, steps=40, random_start=True),
+		atk = config['attack'],
 		**config
 	)
 
+	config['scheduler'].step()
 	torch.save(m.state_dict(), "checkpoints_/" + writer.log_dir.split('/')[-1] + f"_{epoch:03}.pt")
 
 print(m)
--- experiments/e2.py
+++ experiments/e2.py
@@ -11,10 +11,10 @@ from utils import nets, datasets, iterate, misc
 config = {
 	'dataset':'SVHN',
 	'training_step':'trades_step',
-	'batch_size':32,
+	'batch_size':128,
 	'optimizer':'SGD',
-	'optimizer_config':{# Adadelta lr = 1; AdamW lr = 1e-3, weight_decay = 0
-		'lr':0.1,
+	'optimizer_config':{
+		'lr':1,
 		'momentum':0.9,
 		'weight_decay':2e-4,
 	},
@@ -23,14 +23,32 @@ config = {
 		'milestones':[75,90,100],
 		'gamma':0.1
 	},
-# 	'noise_level':0.6,
+	# 'noise_level':0.6,
 	'sample_':'sample_uniform_linf_with_clamp',
-	'num':50,
-	'eps':8/255, #PGD
-	'alpha':0.003,
-	'steps':20,
+	'num':50,	
+	'eps':8/255,
+	'attack':'PGD',
+	'attack_config':{
+		'eps':8/255,
+		'alpha':0.003,
+		'steps':20,
+		'random_start':False,
+	},
+	# 'attack':'PGDL2',
+	# 'attack_config':{
+	# 	'eps':0.5, #PGD
+	# 	'alpha':0.2,
+	# 	'steps':40,
+	# 	'random_start':True,
+	# }
 	'microbatch_size':10000,
 	'threshold':0.95,
+	'adversarial':'TPGD',
+	'adversarial_config':{
+		'eps':8/255,
+		'alpha':2/255,
+		'steps':10,
+	},
 	'device':'cuda',
 	'validation_step':'augmented_step',
 	'attacked_step':'attacked_step'
@@ -52,16 +70,19 @@ for k, v in config.items():
 		config[k] = vars(steps)[v]
 	elif k == 'sample_':
 		config[k] = vars(sampling)[v]
-	if k == 'optimizer':
+	elif k == 'optimizer':
 		config[k] = vars(torch.optim)[v](m.parameters(), **config[k+'_config'])
 		config['scheduler'] = vars(torch.optim.lr_scheduler)[config['scheduler']](config[k], **config['scheduler_config'])
+	elif k == 'adversarial' or k == 'attack':
+		config[k] = vars(torchattacks)[v](m, **config[k+'_config'])
+		
 
 for epoch in range(300):
 	iterate.train(m,
 		train_set = train_set,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.TPGD(m, eps=config['eps'], alpha=0.1, steps=7),
+		atk = config['adversarial'],
 		**config
 	)
 
@@ -69,8 +90,7 @@ for epoch in range(300):
 		val_set = val_set,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.PGD(m, eps=config['eps'], alpha=config['alpha'], steps=config['steps'], random_start=False),
-	#     atk = torchattacks.PGDL2(m, eps=0.5, alpha=0.2, steps=40, random_start=True),
+		atk = config['attack'],
 		**config
 	)
 
