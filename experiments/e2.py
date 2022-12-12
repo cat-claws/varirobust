f643198eb51819a357c11f70b27eb30c6d49d865
zhangruihan1 <zrh15832168740@gmail.com>
2022-12-12 15:43:11 +0800

    scheduler

--- experiments/e2.py
+++ experiments/e2.py
@@ -11,21 +11,30 @@ from utils import nets, datasets, iterate, misc
 config = {
 	'dataset':'SVHN',
 	'training_step':'trades_step',
-	'optimizer':'SGD',
-	'lr':0.01,
-	'momentum':0.9,
-	'weight_decay':3.5e-3,
 	'batch_size':32,
+	'optimizer':'SGD',
+	'optimizer_config':{# Adadelta lr = 1; AdamW lr = 1e-3, weight_decay = 0
+		'lr':0.1,
+		'momentum':0.9,
+		'weight_decay':2e-4,
+	},
+	'scheduler':'MultiStepLR',
+	'scheduler_config':{
+		'milestones':[75,90,100],
+		'gamma':0.1
+	},
 # 	'noise_level':0.6,
 	'sample_':'sample_uniform_linf_with_clamp',
 	'num':50,
-	'eps':8/255,
+	'eps':8/255, #PGD
+	'alpha':0.003,
+	'steps':20,
 	'microbatch_size':10000,
 	'threshold':0.95,
 	'device':'cuda',
 	'validation_step':'augmented_step',
 	'attacked_step':'attacked_step'
-	}
+}
 
 train_set, val_set, channel = misc.auto_sets(config['dataset'])
 m = nets.auto_net(channel).cuda()
@@ -36,27 +45,20 @@ writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config[
 import json
 with open("checkpoints/configs.json", 'a') as f:
 	f.write(json.dumps({**{'run':writer.log_dir.split('/')[-1]}, **config}) + '\n')
-
-if config['optimizer'] == 'Adadelta':
-	optimizer = torch.optim.Adadelta(m.parameters(), lr = config['lr']) #lr = 1
-elif config['optimizer'] == 'Adam':
-	optimizer = torch.optim.Adam(m.parameters(), lr = config['lr']) #lr = 1e-3/128
-elif config['optimizer'] == 'SGD':
-	optimizer = optim.SGD(m.parameters(), lr=config['lr'], momentum=config['momentum'], weight_decay=config['weight_decay'])  
-elif config['optimizer'] == 'AdamW':
-	optimizer = torch.optim.AdamW(m.parameters(), lr=config['lr'], weight_decay=config['weight_decay']) #lr = 1e-3, weight_decay = 0
+	print(json.dumps(config, indent=4))
 
 for k, v in config.items():
 	if k.endswith('_step'):
 		config[k] = vars(steps)[v]
 	elif k == 'sample_':
 		config[k] = vars(sampling)[v]
-
+	if k == 'optimizer':
+		config[k] = vars(torch.optim)[v](m.parameters(), **config[k+'_config'])
+		config['scheduler'] = vars(torch.optim.lr_scheduler)[config['scheduler']](config[k], **config['scheduler_config'])
 
 for epoch in range(300):
 	iterate.train(m,
 		train_set = train_set,
-		optimizer = optimizer,
 		epoch = epoch,
 		writer = writer,
 		atk = torchattacks.TPGD(m, eps=config['eps'], alpha=0.1, steps=7),
@@ -67,11 +69,12 @@ for epoch in range(300):
 		val_set = val_set,
 		epoch = epoch,
 		writer = writer,
-		atk = torchattacks.PGD(m, eps=config['eps'], alpha=0.003, steps=20, random_start=False),
+		atk = torchattacks.PGD(m, eps=config['eps'], alpha=config['alpha'], steps=config['steps'], random_start=False),
 	#     atk = torchattacks.PGDL2(m, eps=0.5, alpha=0.2, steps=40, random_start=True),
 		**config
 	)
 
+	config['scheduler'].step()
 	torch.save(m.state_dict(), "checkpoints_/" + writer.log_dir.split('/')[-1] + f"_{epoch:03}.pt")
 
 print(m)
