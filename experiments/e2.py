1f4b1be29584ff31005353e7326707318fc748d3
zhangruihan1 <zrh15832168740@gmail.com>
2022-12-14 04:20:50 +0800

    cifar10

--- experiments/e2.py
+++ experiments/e2.py
@@ -16,7 +16,7 @@ config = {
 	'optimizer':'SGD',
 	'optimizer_config':{
 		'lr':1e-2,
-		'momentum':0.0,
+		'momentum':0.5,
 		'weight_decay':2e-4,
 	},
 	'scheduler':'MultiStepLR',
@@ -62,7 +62,7 @@ config = {
 
 train_set, val_set, channel = misc.auto_sets(config['dataset'])
 m = nets.auto_net(channel).cuda()
-# m.load_state_dict(torch.load('checkpoints_/Dec12_22-52-07_ruihan-MS-7B23_SVHN_ResNet_trades_step_090.pt'))
+m.load_state_dict(torch.load('checkpoints_/Dec12_22-52-07_ruihan-MS-7B23_SVHN_ResNet_trades_step_090.pt'))
 
 writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config['training_step']}")
 # writer.add_hparams(config, {})
