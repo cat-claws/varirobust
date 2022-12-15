2b4242183aa7e773c4d9cc28b15560fa9ff3c0a7
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-15 21:24:13 +0800

    Update e3.py

--- experiments/e3.py
+++ experiments/e3.py
@@ -61,8 +61,10 @@ config = {
 }
 
 train_set, val_set, channel = misc.auto_sets(config['dataset'])
-m = nets.auto_net(channel).cuda()
+# m = nets.auto_net(channel).cuda()
 # m.load_state_dict(torch.load('checkpoints_/Dec12_22-52-07_ruihan-MS-7B23_SVHN_ResNet_trades_step_090.pt'))
+import pytorchcv
+m = pytorchcv.model_provider.get_model(f"resnet20_{config['dataset'].lower()}", pretrained=True)
 
 writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config['training_step']}")
 # writer.add_hparams(config, {})
