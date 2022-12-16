f75cd943aee30832425ca06c96aa88ae39273ab3
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-17 00:28:42 +0800

    Update e2.py

--- experiments/e2.py
+++ experiments/e2.py
@@ -65,6 +65,7 @@ train_set, val_set, channel = misc.auto_sets(config['dataset'])
 # m.load_state_dict(torch.load('checkpoints/Dec14_02-53-38_ruihan-MS-7B23_SVHN_ResNet_trades_step_005.pt'))
 import pytorchcv.model_provider
 m = pytorchcv.model_provider.get_model(f"resnet20_{config['dataset'].lower()}", pretrained=False).to(config['device'])
+misc.weight_reset(m)
 
 writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config['training_step']}")
 # writer.add_hparams(config, {})
