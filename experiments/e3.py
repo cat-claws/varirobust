1919396b0353d273e0e2e3177178e70e65f78079
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-16 16:39:37 +0800

    Update e3.py

--- experiments/e3.py
+++ experiments/e3.py
@@ -19,16 +19,16 @@ config = {
 		'momentum':0.9,
 		'weight_decay':2e-4,
 	},
-	'scheduler':'MultiStepLR',
-	'scheduler_config':{
-		'milestones':[75,90,105, 120, 135, 150],
-		'gamma':0.1
-	},
-# 	'scheduler':'StepLR',
+# 	'scheduler':'MultiStepLR',
 # 	'scheduler_config':{
-# 		'step_size':15,
-# 		'gamma':0.1,
+# 		'milestones':[75,90,105, 120, 135, 150],
+# 		'gamma':0.1
 # 	},
+	'scheduler':'StepLR',
+	'scheduler_config':{
+		'step_size':15,
+		'gamma':0.1,
+	},
 	# 'noise_level':0.6,
 	'sample_':'sample_uniform_linf_with_clamp',
 	'num':50,	
