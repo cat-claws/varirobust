0ef76ef1969916c3497f138d4e103c62289aa7f8
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-18 19:26:01 +0800

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -17,12 +17,12 @@ class Ensemble(torch.nn.Module):
 	def forward(self, x):
 		return (self.m1(x) + self.m2(x))/2
 
-class SampleEnsemble(torch.nn.Module):
-	def __init__(self, m, **kw):
-		super(SampleEnsemble, self).__init__()
-		self.m = m
-		self.kw = forward_samples(m, x, **kw)
-
-	def forward(self, x, n_neighb = -1, batch_size = -1):
-		forward_samples(self.m, self.sampling, x, self.eps, n_neighb, batch_size)
-			return sum(outputs) / n_neighb
+class SE(torch.nn.Module): # sample ensemble
+	def __init__(self, net, **kw):
+		super(SE, self).__init__()
+		self.net = net
+		self.kw = kw
+		
+	def forward(self, x):
+		outputs = forward_samples(self.net, x, **self.kw)
+		return torch.nn.functional.normalize(outputs, p=1, dim=1).mean(dim = 0)
