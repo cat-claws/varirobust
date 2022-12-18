c8db1aa42ad61cc0b3994d081a6bbad368c9ff56
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-12-18 20:01:59 +0800

    Update e2_.py

--- experiments/e2_.py
+++ experiments/e2_.py
@@ -7,73 +7,47 @@ import torchattacks
 import steps
 import sampling
 from utils import nets, datasets, iterate, misc
+from ensemble import SE
 
-config = {
-	'dataset':'SVHN',
-	'training_step':'our_step',
-	'z':1,
-	'batch_size':32,
-	'optimizer':'Adadelta',
-	'optimizer_config':{
-		'lr':1,
-# 		'momentum':0.5,
-# 		'weight_decay':2e-4,
-	},
-	'scheduler':'MultiStepLR',
-	'scheduler_config':{
-		'milestones':[75,90,105, 120, 135, 150],
-		'gamma':1
-	},
-# 	'scheduler':'StepLR',
-# 	'scheduler_config':{
-# 		'step_size':15,
-# 		'gamma':0.1,
+import argparse
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--run', type=str, default='Dec18_11-01-23_vm1_CIFAR10_ResNet_our_step')
+# parser.add_argument('--test_attacks', type=str, nargs='+', default=['PGD_Linf'])
+# parser.add_argument('--hparams_seed', type=int, default=0, help='Seed for hyperparameters')
+args = parser.parse_args()
+
+import pandas as pd
+
+df = pd.read_json("checkpoints/configs.json", lines = True)
+config = next(iter((df.loc[df['run'] == args.run]).to_dict('index').values()))
+config.update({
+	'num':5,	
+# 	'attack':'PGD',
+# 	'attack_config':{
+# 		'eps':8/255,
+# 		'alpha':1/255,
+# 		'steps':20,
+# 		'random_start':False,
 # 	},
-	# 'noise_level':0.6,
-	'sample_':'sample_uniform_linf_with_clamp',
-	'num':50,	
-	'eps':8/255,
-	'attack':'PGD',
-	'attack_config':{
-		'eps':8/255,
-		'alpha':1/255,
-		'steps':20,
-		'random_start':False,
-	},
-	# 'attack':'PGDL2',
-	# 'attack_config':{
-	# 	'eps':0.5, #PGD
-	# 	'alpha':0.2,
-	# 	'steps':40,
-	# 	'random_start':True,
-	# }
-	'microbatch_size':10000,
-	'threshold':0.95,
-	'adversarial':'TPGD',
-	'adversarial_config':{
-		'eps':8/255,
-		'alpha':2/255,
-		'steps':10,
-	},
-	'device':'cuda',
-	'validation_step':'augmented_step',
-	'attacked_step':'attacked_step'
-}
+})
+print(json.dumps(config, indent=4))
 
-train_set, val_set, channel = misc.auto_sets(config['dataset'])
-# m = nets.auto_net(channel).cuda()
-# m.load_state_dict(torch.load('checkpoints/Dec14_02-53-38_ruihan-MS-7B23_SVHN_ResNet_trades_step_005.pt'))
-import pytorchcv.model_provider
-m = pytorchcv.model_provider.get_model(f"resnet20_{config['dataset'].lower()}", pretrained=False).to(config['device'])
-m.apply(misc.weight_init)
 
-writer = SummaryWriter(comment = f"_{config['dataset']}_{m._get_name()}_{config['training_step']}")
+
+_, val_set, channel = misc.auto_sets(config['dataset'])
+
+if 'CIFARResNet' not in config['run']:
+	m = SE(nets.auto_net(channel).cuda(), **config)
+else:
+	import pytorchcv.model_provider
+	m = SE(pytorchcv.model_provider.get_model(f"resnet20_{config['dataset'].lower()}", pretrained=False).to(config['device']), **config)
+
+m.net.load_state_dict(torch.load("checkpoints_/" + config['run'] + f"_{0:03}.pt"))
+
+writer = SummaryWriter(comment = f"_SE_{config['dataset']}_{m._get_name()}_{config['training_step']}")
 # writer.add_hparams(config, {})
 
-import json
-with open("checkpoints/configs.json", 'a') as f:
-	f.write(json.dumps({**{'run':writer.log_dir.split('/')[-1]}, **config}) + '\n')
-	print(json.dumps(config, indent=4))
+
 
 for k, v in config.items():
 	if k.endswith('_step'):
@@ -88,14 +62,8 @@ for k, v in config.items():
 		
 
 for epoch in range(300):
-	if epoch > 0:
-		iterate.train(m,
-			train_set = train_set,
-			epoch = epoch,
-			writer = writer,
-			atk = config['adversarial'],
-			**config
-		)
+
+	m.net.load_state_dict(torch.load("checkpoints_/" + config['run'] + f"_{epoch:03}.pt"))
 
 	iterate.attack(m,
 		val_set = val_set,
@@ -105,16 +73,7 @@ for epoch in range(300):
 		**config
 	)
 
-	torch.save(m.state_dict(), "checkpoints_/" + writer.log_dir.split('/')[-1] + f"_{epoch:03}.pt")
 
-print(m)
 
-outputs = iterate.predict(m,
-	steps.predict_step,
-	val_set = val_set,
-	**config
-)
-
-# print(outputs.keys(), outputs['predictions'])
 writer.flush()
 writer.close()
